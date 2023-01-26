5935bbd8a7e93cfb4227a72cb8f4056c5143b279
dropsy <dropsy422@gmail.com>
2023-01-26 22:47:11 +0600

    .

--- ComposerTrack/Views/EditorWindow.xaml.cs
+++ ComposerTrack/Views/EditorWindow.xaml.cs
@@ -4,20 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Wpf.Ui.Controls;
 
 namespace ComposerTrack.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для EditorWindow.xaml
-    /// </summary>
     public partial class EditorWindow : Window
     {
         public EditorWindow()
