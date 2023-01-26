1d675c463ba2bed19339c42e5fd978fad5a1cc1a
dropsy <dropsy422@gmail.com>
2023-01-26 20:18:37 +0600

    .

---
+++ ComposerTrack.API/Extension.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+namespace ComposerTrack.API
+{
+    public abstract class Extension
+    {
+        public string Name { get; private set; }
+        public string ID { get; private set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+        public Extension(string name, string id)
+        {
+        }
+    }
+}
