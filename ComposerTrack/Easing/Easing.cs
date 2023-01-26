6e923fe65ecbd8e7b9029b5f24b89ae5ba92e2ec
dropsy <dropsy422@gmail.com>
2023-01-26 17:44:07 +0600

    .

--- ComposerTrack/Easing/Easing.cs
+++ ComposerTrack/Easing/Easing.cs
@@ -6,63 +6,159 @@ using System.Threading.Tasks;
 
 namespace ComposerTrack.Easing
 {
+    public enum ListEase : byte
+    {
+        Linear,
+        InQuad,
+        OutQuad,
+        InOutQuad,
+        InCubic,
+        OutCubic,
+        InOutCubic,
+        InQuart,
+        OutQuart,
+        InOutQuart,
+        InQuint,
+        OutQuint,
+        InOutQuint,
+        InSine,
+        OutSine,
+        InOutSine,
+        InExpo,
+        OutExpo,
+        InOutExpo,
+        InCirc,
+        OutCirc,
+        InOutCirc,
+        InElastic,
+        OutElastic,
+        InOutElastic,
+        InBack,
+        OutBack,
+        InOutBack,
+        InBounce,
+        OutBounce,
+        InOutBounce,
+    }
+
     public static class Easing
     {
-        public static float Linear(float t) => t;
+        public static float Linear(float t) { return t; }
 
-        public static float InQuad(float t) => t * t;
-        public static float OutQuad(float t) => 1 - InQuad(1 - t);
+        public static float InQuad(float t)
+        {
+            return t * t;
+        }
+
+        public static float OutQuad(float t)
+        {
+            return 1 - InQuad(1 - t);
+        }
+
         public static float InOutQuad(float t)
         {
             if (t < 0.5) return InQuad(t * 2) / 2;
             return 1 - InQuad((1 - t) * 2) / 2;
         }
 
-        public static float InCubic(float t) => t * t * t;
-        public static float OutCubic(float t) => 1 - InCubic(1 - t);
+        public static float InCubic(float t)
+        {
+            return t * t * t;
+        }
+
+        public static float OutCubic(float t)
+        {
+            return 1 - InCubic(1 - t);
+        }
+
         public static float InOutCubic(float t)
         {
             if (t < 0.5) return InCubic(t * 2) / 2;
             return 1 - InCubic((1 - t) * 2) / 2;
         }
 
-        public static float InQuart(float t) => t * t * t * t;
-        public static float OutQuart(float t) => 1 - InQuart(1 - t);
+        public static float InQuart(float t)
+        {
+            return t * t * t * t;
+        }
+
+        public static float OutQuart(float t)
+        {
+            return 1 - InQuart(1 - t);
+        }
+
         public static float InOutQuart(float t)
         {
             if (t < 0.5) return InQuart(t * 2) / 2;
             return 1 - InQuart((1 - t) * 2) / 2;
         }
 
-        public static float InQuint(float t) => t * t * t * t * t;
-        public static float OutQuint(float t) => 1 - InQuint(1 - t);
+        public static float InQuint(float t)
+        {
+            return t * t * t * t * t;
+        }
+
+        public static float OutQuint(float t)
+        {
+            return 1 - InQuint(1 - t);
+        }
+
         public static float InOutQuint(float t)
         {
             if (t < 0.5) return InQuint(t * 2) / 2;
             return 1 - InQuint((1 - t) * 2) / 2;
         }
 
-        public static float InSine(float t) => (float)-Math.Cos(t * Math.PI / 2);
-        public static float OutSine(float t) => (float)Math.Sin(t * Math.PI / 2);
-        public static float InOutSine(float t) => (float)(Math.Cos(t * Math.PI) - 1) / -2;
+        public static float InSine(float t)
+        {
+            return (float)-Math.Cos(t * Math.PI / 2);
+        }
 
-        public static float InExpo(float t) => (float)Math.Pow(2, 10 * (t - 1));
-        public static float OutExpo(float t) => 1 - InExpo(1 - t);
+        public static float OutSine(float t)
+        {
+            return (float)Math.Sin(t * Math.PI / 2);
+        }
+
+        public static float InOutSine(float t)
+        {
+            return (float)(Math.Cos(t * Math.PI) - 1) / -2;
+        }
+
+        public static float InExpo(float t)
+        {
+            return (float)Math.Pow(2, 10 * (t - 1));
+        }
+
+        public static float OutExpo(float t)
+        {
+            return 1 - InExpo(1 - t);
+        }
+
         public static float InOutExpo(float t)
         {
             if (t < 0.5) return InExpo(t * 2) / 2;
             return 1 - InExpo((1 - t) * 2) / 2;
         }
 
-        public static float InCirc(float t) => -((float)Math.Sqrt(1 - t * t) - 1);
-        public static float OutCirc(float t) => 1 - InCirc(1 - t);
+        public static float InCirc(float t)
+        {
+            return -((float)Math.Sqrt(1 - t * t) - 1);
+        }
+
+        public static float OutCirc(float t) {
+            return 1 - InCirc(1 - t);
+        }
         public static float InOutCirc(float t)
         {
             if (t < 0.5) return InCirc(t * 2) / 2;
             return 1 - InCirc((1 - t) * 2) / 2;
         }
 
-        public static float InElastic(float t) => 1 - OutElastic(1 - t);
+        public static float InElastic(float t)
+        {
+            return 1 - OutElastic(1 - t);
+        }
+
         public static float OutElastic(float t)
         {
             float p = 0.3f;
@@ -79,17 +175,24 @@ namespace ComposerTrack.Easing
             float s = 1.70158f;
             return t * t * ((s + 1) * t - s);
         }
-        public static float OutBack(float t) => 1 - InBack(1 - t);
+        public static float OutBack(float t)
+        {
+            return 1 - InBack(1 - t);
+        }
+
         public static float InOutBack(float t)
         {
             if (t < 0.5) return InBack(t * 2) / 2;
             return 1 - InBack((1 - t) * 2) / 2;
         }
 
-        public static float InBounce(float t) => 1 - OutBounce(1 - t);
+        public static float InBounce(float t)
+        {
+             return 1 - OutBounce(1 - t);
+        }
         public static float OutBounce(float t)
         {
-            float div = 2.75f;
+            var div = 2.75f;
             float mult = 7.5625f;
 
             if (t < 1 / div)
